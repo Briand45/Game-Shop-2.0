8289c781291e2cf2ed5180c0ae59b4265fc57307
Briand45 <brian.banos@educa.madrid.org>
2026-09-17 16:23:05 +0200

    Se crea la clase Purchase

--- src/tiendajuegosB/Purchase.java
+++ src/tiendajuegosB/Purchase.java
@@ -1,8 +1,37 @@
 package tiendajuegosB;
 
 public class Purchase {
-private int customer;
-private String game;
-private int copias;
 
+    private Customer customer;
+    private Game game;
+    private int quantity;
+    private double totalPrice;
+
+    public Purchase(Customer customer, Game game, int quantity) {
+        this.customer = customer;
+        this.game = game;
+        this.quantity = quantity;
+        this.totalPrice = game.getPrecio() * quantity;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
 }
+
+/*Cree la clase Purchase, con las variables y cree un constructor, con el getPrecio de la
+ * clase game y luego lo multiplico por la cantidad que es la variable quantity */
+
+
